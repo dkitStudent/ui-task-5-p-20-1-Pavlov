40a43cdbc4921b19b7f6f61c546b993a507b8098
dkitStudent <dkitneeds@gmail.com>
2022-10-26 02:58:45 +0300

    Добавьте файлы проекта.

---
+++ ui-task-5-p-20-1-Pavlov/View/UserControls/TimerSettingsControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ui_task_5_p_20_1_Pavlov.View.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TimerSettingsControl.xaml
+    /// </summary>
+    public partial class TimerSettingsControl : UserControl
+    {
+        public TimerSettingsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
